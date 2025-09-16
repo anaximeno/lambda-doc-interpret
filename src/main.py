dae0994f5c8f460cf01b9eb605d6da2b84a00535
anaximeno <anaximenobrito@gmail.com>
2025-09-16 18:01:21 +0000

    chore: Improve files + folder default formatting

--- src/main.py
+++ src/main.py
@@ -2,19 +2,13 @@ from fastapi import FastAPI
 from mangum import Mangum
 
 app = FastAPI()
+lambda_handler = Mangum(app)
 
-
-@app.get("/")
-def get_root():
+@app.get("/hello")
+async def get_root():
     return {"Hello": "World"}
 
 
 @app.get("/test")
-def get_test():
+async def get_test():
     return {"hell": "yeah"}
-
-lambda_handler = Mangum(app, lifespan="off")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app:app", port=5000, log_level="info", reload=True)
