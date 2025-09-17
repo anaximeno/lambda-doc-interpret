67225ec990f56258098999b7cb1b88704fbb4f8f
anaximeno <anaximenobrito@gmail.com>
2025-09-17 11:29:40 +0000

    chore: Better handle in case of errors

--- src/routes/extract.py
+++ src/routes/extract.py
@@ -8,10 +8,20 @@ extraction_service = DocumentExtractionService()
 
 @router.post("")
 async def extract(body: ExtractionInfoDTO):
-    extracted_contents = extraction_service.extract_contents(body.pdf_url, body.case_id)
-    document_extraction = extraction_service.save_case_extraction(body.case_id, extracted_contents)
-    data = document_extraction.model_dump_json()
-    return data
+    document_extraction = None
+    message = "Content not extracted."
 
+    try:
+        extracted_contents = extraction_service.extract_contents(body.pdf_url, body.case_id)
+        document_extraction = extraction_service.save_case_extraction(body.case_id, extracted_contents)
+    except Exception as e:
+        message = f"Process couldn't be handled successfuly, exception: {e}"
+
+    if document_extraction is not None:
+        data = document_extraction.model_dump_json()
+        return data
+
+    return { "message": message }
+    
 
 __all__ = ["router"]
