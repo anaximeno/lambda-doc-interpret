f1f1b6881a4414e916dbb0f9b1ea8b6215d95d77
anaximeno <anaximenobrito@gmail.com>
2025-09-17 11:29:54 +0000

    chore: Add note regarding bd host

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -6,5 +6,5 @@ class Settings:
     POSTGRES_USER = 'interpreter'
     POSTGRES_PASSWORD = 'passw0rd'
     POSTGRES_DB = 'interpretationsdb'
-    POSTGRES_HOST = '127.0.0.1'
+    POSTGRES_HOST = '192.168.122.254' # NOTE: localhost wasn't working for some reason, therefore using my local vm local ip addr
     POSTGRES_PORT = '5432'
